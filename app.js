51b3610da72d96ebe4e963dfb452c77052db8e1e
Tobias Wupperfeld <tobias@wupperfeld.org>
2020-08-07 11:25:36 +0200

    update app

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://limitless-forest-94024.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
